7f8bf9347b516fee3ac49e06bb8be8755aeacb11
Reimu <nm5reimu@gmail.com>
2020-10-09 12:34:05 +0300

    feat: added crabon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity != 'string') return false;
+  let sampleActivityNumber = parseFloat(sampleActivity);
+  if (isNaN(sampleActivityNumber)) return false;
+  if (sampleActivityNumber > 15 || sampleActivityNumber <= 0) return false;
+  return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivityNumber))/(0.693 / HALF_LIFE_PERIOD));
 };
