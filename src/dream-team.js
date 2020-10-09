c602cc60ffacb4986dc821dbc31d0fc2c506a1e8
Reimu <nm5reimu@gmail.com>
2020-10-09 12:34:42 +0300

    feat: added dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(Array.isArray(members))) return false;
+  let team = [];
+  members.map(name => {
+    if(typeof name === 'string' && name !== '') team.push(name.trim()[0].toUpperCase());    
+  })
+  return team.sort().join("");
 };
