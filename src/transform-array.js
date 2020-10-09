f5f0c1476653f2b1563413108a272561df8dc620
Reimu <nm5reimu@gmail.com>
2020-10-09 12:36:44 +0300

    feat: added transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function trresultform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  let res = arr.slice('');
+    for (let i = 0; i < res.length; i++) {
+      if (res[i] === '--discard-next') res.splice(i, 2, undefined)
+        else if (res[i] === '--discard-prev') i === 0 ? res.splice(i, 1) : res.splice(i - 1, 2, undefined)
+        else if (res[i] === '--double-next') res.splice(i, 1, res[i + 1])
+        else if (res[i] === '--double-prev') res.splice(i, 1, res[i - 1])
+    }
+    return res.filter(e => e !== undefined);
 };
